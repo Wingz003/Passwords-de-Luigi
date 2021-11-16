06446b890fc3acceb1e56f1df4d726d85e239337
Wingz003 <Lsanti618@gmail.com>
2021-11-16 01:35:14 -0600

    added for loop and declared variable that contains Math.floor

--- script.js
+++ script.js
@@ -2,28 +2,37 @@
 let arr = [];
 let generateBtn = document.querySelector("#generate");
 function generatePassword() {
- let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
- let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
- let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
- let symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
- let allCodes = [...lowerCase, ...upperCase, ...number, ...symbols] 
-
-
+  let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
+  let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  let symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
+  let allCodes = [...lowerCase, ...upperCase, ...number, ...symbols] 
   
+  
+  for(let i = 0; i < allCodes.length; i++) {
+    
+    let pick = Math.floor(Math.random() * allCodes.length)
+    arr.push(allCodes[pick]);
+    
+  }
+  
+  return arr.join("")
 }
+
+
 // Write password to the #password input
 function writePassword() {
- 
+  
+  
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
   passwordText.value = password;
+  password.value.event.preventDefault();
   
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// 1. prompt the user/
 //   a. Password Length 8 < 128
 //   b. Uppercase, Lowercase, Numbers, Symbols
 // 2. validate the input
